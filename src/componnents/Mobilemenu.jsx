ba19163a23bc0d84ea1ca03fc4a47233b9752a2b
ys936137-byte <ys936137@gmail.com>
2026-05-09 01:29:32 +0530

    change css in  mobilemenu

--- src/componnents/Mobilemenu.jsx
+++ src/componnents/Mobilemenu.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router'
 
 const Mobilemenu = () => {
   return (
-    <div className='h-10 w-full md:hidden fixed bottom-0 inset-10 flex justify-center items-center shadow-2xl shadow-gray-950'>
+    <div className='bg-white h-10 w-full md:hidden fixed bottom-0 flex justify-center items-center shadow-2xl shadow-gray-950'>
       <nav className='mt-6 h-8 w-full'>
         <ul className='flex justify-between items-center ml-5 mr-5 text-sm'>
               <NavLink to="/" className={({ isActive }) => `${isActive ? "border-b-2 border-red-600 font-bold" : "text-black"}`}><li>Home</li></NavLink>
